7c6ff49d1a967150af4da090648e04f32529efe6
Arjunsivakumar28 <72465509+Arjunsivakumar28@users.noreply.github.com>
2025-01-25 14:36:35 -0600

    Add in frontend components and work (#6)
    Co-authored-by: Carson Bates <carson.d.bates.27@dartmouth.edu>
    Co-authored-by: carsonbates913 <153021111+carsonbates913@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,17 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 import LandingPage from './pages/LandingPage/LandingPage'
 import ApiClassifier from './pages/ApiPage/ApiClassifier'
+import SignInPage from './pages/SignInPage/SignInPage.jsx';
 import Scanner from './pages/Scanner/Scanner'
 import ChengModel from './pages/ChengModel/ChengModel';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />}/>
+        <Route path="/" element={<SignInPage />}/>
         <Route path="/apiClassifier" element={<ApiClassifier />} />
         <Route path="/scanner" element={<Scanner />} />
         <Route path="chengMl" element={<ChengModel />} />
