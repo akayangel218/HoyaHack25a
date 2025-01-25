0be6e2a2357b56655bca838b6751a0d629d4c75f
Arjun <arjuns8@illinois.edu>
2025-01-25 13:20:22 -0600

    Cheng's model initial commit

--- frontend/src/pages/ChengMLModel/ChengClassifier.jsx
+++ frontend/src/pages/ChengMLModel/ChengClassifier.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 
-const ApiClassifier = () => {
+const ChengClassifier = () => {
   const [image, setImage] = useState(null);
   const [huggingClassification, setHuggingClassification] = useState("");
 
   async function handleImageUpload(event) {
     const file = event.target.files[0];
-    // console.log(file)
     if (file) {
       const imageUrl = URL.createObjectURL(file);
       setImage(imageUrl);
@@ -48,20 +47,20 @@ const ApiClassifier = () => {
 
 
 async function huggingFaceQuery(imageFile) {
-	const data = imageFile
-	const response = await fetch(
-		"https://api-inference.huggingface.co/models/Anwarkh1/Skin_Cancer-Image_Classification",
-		{
-			headers: {
-				Authorization: "Bearer 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",
-				"Content-Type": "application/json",
-			},
-			method: "POST",
-			body: data,
-		}
-	);
-	const result = await response.json();
-	return result;
+    const data = imageFile
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/Anwarkh1/Skin_Cancer-Image_Classification",
+        {
+            headers: {
+                Authorization: "Bearer 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",
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: data,
+        }
+    );
+    const result = await response.json();
+    return result;
 }
 
-export default ApiClassifier;
+export default ChengClassifier;
